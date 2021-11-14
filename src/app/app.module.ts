3e0e279223c00342d4a2403155fa3f90be78f90f
Ömer Faruk Korkmaz <korkmazfaruk84@gmail.com>
2021-11-14 19:35:11 +0300

    Changes: Filter have been added to Dashboard Header has been added to Dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,7 +33,8 @@ import { PhotofilterbycarPipe } from './pipes/photofilterbycar.pipe';
     AppRoutingModule,
     NgbModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
